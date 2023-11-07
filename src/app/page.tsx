dfb2385cb2919b6104ed4e5f0b973bbd3174fc4b
smritiiin <smritynpl@gmail.com>
2023-11-07 12:30:46 +0545

    Modified HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
-  return (
-    <h1>Co-Travel Nepal</h1>
-  )
+  return <h1>Co-Travel Nepal. This is a travel app</h1>;
 }
