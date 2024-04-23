71fd0eb6ed570d3cbc4b288b72b207e366a7dd13
smritiiin <smritynpl@gmail.com>
2024-04-23 20:24:19 +0545

    PWA implemented

--- next.config.js
+++ next.config.js
@@ -1,11 +1,27 @@
 // /** @type {import('next').NextConfig} */
-// const nextConfig = {}
+
+const withPWA = require('next-pwa')({
+  dest: 'public',register: true,
+    skipWaiting: true,
+})
+
+module.exports = withPWA({
+  pwa:{
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+  },
+  reactStrictMode: true,
+  images: {
+    domains: ["localhost", "openweathermap.org"],
+  },
+});
 
 // module.exports = nextConfig
 
 // next.config.js
-module.exports = {
-  images: {
-    domains: ["localhost", "openweathermap.org"],
-  },
-};
+// module.exports = {
+//   images: {
+//     domains: ["localhost", "openweathermap.org"],
+//   },
+// };
