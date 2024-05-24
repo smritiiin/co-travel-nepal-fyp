24e870e3a8ba38fb500ee7efad0cb5b3964dbcee
smritiiin <smritynpl@gmail.com>
2024-05-24 13:42:28 +0545

    Admin naviagtion fixed

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -4,29 +4,67 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { Card, Image, Button, Spacer } from "@nextui-org/react";
 import NavBar from "./NavBar";
-import { useToken } from "@/utils/token";
 import adminAuth from "@/utils/adminAuth";
+import axios from "axios";
 
 const Admin = () => {
+  const [usersCount, setUsersCount] = useState(0);
+  const [agentsCount, setAgentsCount] = useState(0);
+  const [placesCount, setPlacesCount] = useState(0);
+  const [bookingsCount, setBookingsCount] = useState(0);
+
+  useEffect(() => {
+    const fetchCounts = async () => {
+      try {
+        const [usersRes, placesRes, bookingsRes] = await Promise.all([
+          axios.get("http://localhost:8000/api/user/users"),
+          axios.get("http://localhost:8000/api/place/"),
+          axios.get("http://localhost:8000/api/package/booking"),
+        ]);
+
+        setUsersCount(usersRes.data.length);
+        setAgentsCount(1);
+        setPlacesCount(placesRes.data.length);
+        setBookingsCount(bookingsRes.data.length);
+      } catch (error) {
+        console.error("Error fetching counts:", error);
+      }
+    };
+
+    fetchCounts();
+  }, []);
+  const router = useRouter();
   return (
     <div className="flex h-screen">
       <NavBar />
       <div className="w-[75%] h-screen p-8">
         <div className="flex gap-6">
-          <Card className="bg-[#F0F9FF] w-96">
+          <Card
+            className="bg-[#F0F9FF] w-96"
+            isHoverable
+            isPressable
+            onClick={() => router.push("/admin/places")}
+          >
             <div className=" p-4">
               <Image
                 alt="Places"
                 height={50}
                 radius="sm"
                 src="/images/admin/places.svg"
-                width={40}
+                width={30}
               />
               <p className="font-bold text-lg text-[#6C6C6C]"> Places</p>
             </div>
-            <div className="flex justify-end px-6 font-bold text-2xl">246</div>
+            <div className="flex w-full justify-end px-6 font-bold text-2xl">
+              {placesCount}
+            </div>
           </Card>
-          <Card className="bg-[#FEF6FB] w-96">
+          <Card
+            className="bg-[#FEF6FB] w-96"
+            isHoverable
+            isPressable
+            onClick={() => router.push("/admin/users")}
+          >
             <div className=" p-4">
               <Image
                 alt="Users"
@@ -37,9 +75,16 @@ const Admin = () => {
               />
               <p className="font-bold text-lg text-[#6C6C6C]"> Users</p>
             </div>
-            <div className="flex justify-end px-6 font-bold text-2xl">246</div>
+            <div className="flex justify-end px-6 font-bold text-2xl">
+              {usersCount}
+            </div>
           </Card>
-          <Card className="bg-[#FEFBEC] w-96">
+          <Card
+            className="bg-[#FEFBEC] w-96"
+            isHoverable
+            isPressable
+            onClick={() => router.push("/admin/booking")}
+          >
             <div className=" p-4">
               <Image
                 alt="Booking"
@@ -50,9 +95,16 @@ const Admin = () => {
               />
               <p className="font-bold text-lg text-[#6C6C6C]"> Bookings</p>
             </div>
-            <div className="flex justify-end px-6 font-bold text-2xl">246</div>
+            <div className="flex justify-end px-6 font-bold text-2xl">
+              {bookingsCount}
+            </div>
           </Card>
-          <Card className=" bg-gradient-to-b from-[#00B2FE] from-99% to-white to-1% w-96">
+          <Card
+            className=" bg-gradient-to-b from-[#00B2FE] from-99% to-white to-1% w-96"
+            isHoverable
+            isPressable
+            onClick={() => router.push("/admin/agent")}
+          >
             <div className=" p-4">
               <Image
                 alt="Agent"
@@ -63,7 +115,9 @@ const Admin = () => {
               />
               <p className="font-bold text-lg text-[#6C6C6C]"> Agents</p>
             </div>
-            <div className="flex justify-end px-6 font-bold text-2xl">246</div>
+            <div className="flex justify-end px-6 font-bold text-2xl">
+              {agentsCount}
+            </div>
           </Card>
         </div>
       </div>
